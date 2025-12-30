4cf208f78abe74783bc172ada1d2098ea6deb20a
Yunus Emre Zorlu <yunuzorlu@gmail.com>
2025-12-30 15:12:20 +0300

    Initial Commit

---
+++ grindin/grindinApp.swift
@@ -0,0 +1,17 @@
+//
+//  grindinApp.swift
+//  grindin
+//
+//  Created by Yunus Emre Zorlu on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct grindinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
